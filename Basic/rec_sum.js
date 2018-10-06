9131f565f7bf6c08cf72fca3f1d5c60b88748deb
Winston Chan <winston.chan94@gmail.com>
2018-10-05 22:45:49 -0700

    Recsum in js

--- Basic/rec_sum.js
+++ Basic/rec_sum.js
@@ -1,4 +1,4 @@
-//takes an array of integers and returns their sum recursively
+//takes an array of integers and returns their sum recursivelyy
 
 const recSum = (arr) => {
   if (arr.length == 1) {
