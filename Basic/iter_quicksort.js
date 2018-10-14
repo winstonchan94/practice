9cc7de8664aca6b5e633024858c247ebdf0fff9f
Winston Chan <winston.chan94@gmail.com>
2018-10-14 15:32:12 -0700

    Finish iter quicksort

--- Basic/iter_quicksort.js
+++ Basic/iter_quicksort.js
@@ -1,5 +1,35 @@
 const partition = (arr, start, end) => {
+  let pivot = arr[end];
   let pivIdx = start;
-
+  let temp;
+  for (let i = start; i < end; i++) {
+    if (arr[i] <= arr[pivIdx]) {
+      temp = arr[i];
+      arr[i] = arr[pivIdx];
+      arr[pivIdx] = temp;
+      pivIdx++;
+    }
+  }
+  return pivIdx;
 };
-//s
+
+const iterQuicksort = (arr) => {
+  let stack = [];
+  let a = 0;
+  let b = arr.length - 1;
+  let current;
+  let pivIdx;
+  stack.push([a,b]);
+  while (stack.length > 0) {
+    current = stack.pop();
+    a = current[0];
+    b = current[1];
+    pivIdx = partition(arr, a, b);
+    if (pivIdx - 1 > a) {
+      stack.push([a, pivIdx - 1]);
+    }
+    if (pivIdx + 1 < b) {
+      stack.push([pivIdx + 1, b]);
+    }
+  }
+};
