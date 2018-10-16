562e70a95c2b4799c4e6257db2c926515161b19b
Winston Chan <winston.chan94@gmail.com>
2018-10-15 23:40:02 -0700

    Merge two comment

--- Basic/merge_two.js
+++ Basic/merge_two.js
@@ -1,3 +1,5 @@
+//given two sorted arrays, arr1 and arr2, merge the values such that all the values in arr1 are less than those of arr2.
+
 const mergeTwo = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] > arr2[0]) {
