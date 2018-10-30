3a552629866b6342c5766d90586104e9805ddc0b
Winston Chan <winston.chan94@gmail.com>
2018-10-29 23:11:24 -0700

    Do leftmost node tree

---
+++ Basic/bottom_left_most.js
@@ -0,0 +1,53 @@
+function findLeftDepth(root) {
+	if (!root) {
+	   return 0;
+  }
+  if (!root.left) {
+		return 0;
+  }
+
+  let leftDepth = 0;
+
+  if (root.left) {
+  		leftDepth ++;
+  	findLeftDepth(root.left) ;
+  	} else if (root.right) {
+  		leftDepth++;
+  	findLeftDepth(root.right);
+  }
+    return leftDepth;
+}
+
+function findRightDepth(root) {
+	if (!root) {
+	   return 0;
+  }
+  if (!root.left) {
+		return 0;
+  }
+
+  let rightDepth = 0;
+
+  if (root.right) {
+  		rightDepth ++;
+  	findRightDepth(root.right) ;
+  } else if (root.left) {
+  		rightDepth++;
+  	findRightDepth(root.right);
+  }
+    return rightDepth;
+}
+
+function findLeftmost(root) {
+	let left = findLeftDepth(root);
+	let right = findRightDepth(root);
+
+	if (!root.left && !root.right) {
+		return root;
+	}
+  if (left >= right) {
+  		findLeftmost(root.left);
+  }  else {
+  		findLeftmost(root.right);
+  }
+}
