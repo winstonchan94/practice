5539ebf7ca57abaae7f874e3f043914c9818a9fc
Winston Chan <winston.chan94@gmail.com>
2018-11-12 20:39:14 -0800

    Do intersecting arrays

---
+++ Basic/intersectArray.js
@@ -0,0 +1,14 @@
+function intersectArray (arr1, arr2) {
+  let results = [];
+  let seen = {};
+  for (let i; i < arr1.length; i++) {
+    seen[arr1[i]] = false;
+  }
+  for (let j; j < arr2.length; j++) {
+    if (seen[arr2[j]] && seen[arr2[j]] === false) {
+      results.push(seen[arr2[j]]);
+      seen[arr2[j]] = true;
+    }
+  }
+  return results; 
+}
