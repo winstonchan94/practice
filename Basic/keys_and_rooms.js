a9eaed9c77394619893c0e8b57050e898d8cf7ff
Winston Chan <winston.chan94@gmail.com>
2018-11-25 18:50:22 -0800

    Keys n rums

--- Basic/keys_and_rooms.js
+++ Basic/keys_and_rooms.js
@@ -1,4 +1,4 @@
 function keysAndRooms(rooms) {
 
 }
-//given a 2d array of rooms containing keys, 
+//given a 2d array of rooms containing keys, each of which corresponds to a room it can unlock, return true if all rooms are visitable
