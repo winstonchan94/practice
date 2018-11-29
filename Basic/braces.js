82449101b77a87af4d97175dfb8065208d8356a8
Winston Chan <winston.chan94@gmail.com>
2018-11-28 22:22:58 -0800

    Finish a problem

--- Basic/braces.js
+++ Basic/braces.js
@@ -1,7 +1,31 @@
 function braces(arr) {
-
+  let results = [];
+  for (let i = 0; i < arr.length; i++ ) {
+    results.push(isBalanced(arr[i]));
+  }
+  return results; 
 }
 
 function isBalanced(str) {
-  
+  let hash = {
+    "[": "]",
+    "(": ")",
+    "{": "}",
+  };
+  let stack = [];
+  if (str.length % 2 !== 0 || str[str.length - 1] == "[" || str[str.length - 1] == "{" || str[str.length - 1] == "(") {
+    return "NO";
+  }
+  for (let i = 0; i < str.length; i++) {
+    let current = str[i];
+    if (current == "[" || current == "(" || current == "{") {
+      stack.push(current);
+    } else {
+      if (hash[stack.pop()] !== current || stack.length == 0) {
+        return "NO";
+      }
+    }
+  }
+  return "YES";
+
 }
