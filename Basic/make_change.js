c06a2dc5fa5dc0177d5b0c67028825ec3556167c
Winston Chan <winston.chan94@gmail.com>
2018-12-01 23:34:22 -0800

    Make change

--- Basic/make_change.js
+++ Basic/make_change.js
@@ -1,6 +1,7 @@
 function makeChange(total, coins) {
   let answers = new Array(total + 1);
   answers.fill(0);
+  answers[0] = 1;
   for (let coin in coins) {
     for (let i = 0; i < total + 1; i++) {
       if (coin <= i) {
