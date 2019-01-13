ff2f5f0f323c2acb158192bf471fcea09988e7ad
Winston Chan <winston.chan94@gmail.com>
2019-01-12 23:21:40 -0800

    Optimize

--- Basic/caesar_cipher.js
+++ Basic/caesar_cipher.js
@@ -1,4 +1,4 @@
-//given a string and a shift increment return an encoded stringss
+//given a string and a shift increment return an encoded stringsss
 
 const caesarCipher = (str, shift) => {
   let alpha = Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
