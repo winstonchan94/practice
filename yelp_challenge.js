7297ed28a227fb21943f17f671eced66379bb507
Winston Chan <winston.chan94@gmail.com>
2019-02-06 11:49:25 -0800

    Yelp CC fix

---
+++ yelp_challenge.js
@@ -0,0 +1,24 @@
+//find the response rate of a business to user messages.
+const findResponseRate = (bizOwnerId, allMessages) => {
+  let responseHash = {};
+  let totalConvos = 0;
+  let twoWays = 0;
+  for (let message in allMessages) {
+    let messId = message[conversationId];
+    let senderId = message[sender];
+    if (!responseHash[messId]) {
+      responseHash[messId] = 0;
+      totalConvos++;
+    } else if (senderId === bizOwnerId) {
+      responseHash[messId] = 1;
+    }
+  }
+  for (const messId in responseHash) {
+    let response = responseHash[messId];
+    if (response == 1) {
+      twoWays++;
+    }
+  }
+  let answer = (twoWays / totalConvos) * 100;
+  return answer;
+};
