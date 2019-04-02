9c380d6911a2b32d94c4b07a94ac775fc842e060
Winston Chan <winston.chan94@gmail.com>
2019-04-01 21:58:08 -0700

    Do recent pings problem

---
+++ Basic/recentpings.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(val) {
+        this.data = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this._head = null;
+        this._tail = null;
+        this._size = 0;
+    }
+
+    peek(val) {
+        return this._head.data;
+    }
+
+    enqueue(val) {
+        if (this._size === 0) {
+            const node = new Node(val);
+            this._tail = node;
+            this._head = node;
+            this._head.next = this._tail;
+        } else {
+            const last = new Node(val);
+            this._tail.next = last;
+            this._tail = last;
+        }
+
+        this._size += 1;
+    }
+
+    dequeue() {
+        const tmp = this._head;
+
+        if (this._size > 0) {
+            this._size -= 1;
+        }
+
+        if (this._head) {
+            this._head = this._head.next;
+        }
+
+        return tmp;
+    }
+}
+
+class RecentCounter {
+    constructor() {
+        this.pings = new Queue();
+    }
+
+    ping(t) {
+        this.pings.enqueue(t);
+
+        while (this.pings.peek(t) + 3000 < t) {
+            this.pings.dequeue();
+        }
+
+        return this.pings._size;
+    }
+}
