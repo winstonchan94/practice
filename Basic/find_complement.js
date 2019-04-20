e8ee921ee8cd661184ee32b314b8d86fd33987ab
Winston Chan <winston.chan94@gmail.com>
2019-04-19 21:19:30 -0700

    Practice problems

---
+++ Basic/find_complement.js
@@ -0,0 +1,8 @@
+var findComplement = function(num) {
+    var number = num.toString(2);
+    var str = '';
+    for(let i of number) {
+        str += +!(i-0);
+    }
+    return parseInt(str, 2)
+};
