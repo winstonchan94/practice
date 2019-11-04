f580fd3e65282f9fbb84fc66732006498ea21d58
Winston Chan <winston.chan94@gmail.com>
2019-11-04 11:37:43 -0800

    Do max product two words problem

---
+++ Basic/max_product_two_words.js
@@ -0,0 +1,23 @@
+function compare(a,b){
+    for (let i =0; i<a.length; i++){
+       if ( b.includes(a[i])){
+           return false;
+       }
+    }
+    return true;
+}
+
+
+var maxProduct = function(words) {
+    let max = 0;
+    for (let i =0; i<words.length; i++){
+        for (let j=i+1; j<words.length; j++){
+           if( compare(words[i],words[j])) {
+               max = max> words[i].length * words[j].length? max : words[i].length * words[j].length;
+           }
+        }
+
+    }
+    return max;
+
+};
